b68cf987f02a7258ac7b39ca60bd639293ddf6a5
Jonathan Garay <jonathan@garay.mx>
2026-01-09 14:21:10 -0500

    Initial Commit

---
+++ libre/libreApp.swift
@@ -0,0 +1,17 @@
+//
+//  libreApp.swift
+//  libre
+//
+//  Created by Jonathan Garay on 2026-01-09.
+//
+
+import SwiftUI
+
+@main
+struct libreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ libreTests/libreTests.swift
@@ -0,0 +1,17 @@
+//
+//  libreTests.swift
+//  libreTests
+//
+//  Created by Jonathan Garay on 2026-01-09.
+//
+
+import Testing
+@testable import libre
+
+struct libreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
